5c0bbd3d89594f7bab4df721825889f903fd2c47
goni715 <halesgoni715149123@gmail.com>
2025-09-27 09:37:42 +0600

    base_url updated

--- src/redux/features/api/apiSlice.ts
+++ src/redux/features/api/apiSlice.ts
@@ -5,7 +5,7 @@ import { getToken } from "../../../helper/SessionHelper.ts";
 import { ErrorToast } from "../../../helper/ValidationHelper.ts";
 
 // export const baseUrl = "http://localhost:9090/api/v1";
-export const baseUrl = "http://16.171.171.18:9090/api/v1";
+export const baseUrl = "https://backend.onlinecornerstore.com/api/v1";
 
 
 const baseQuery = fetchBaseQuery({
