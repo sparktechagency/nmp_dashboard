aee9df1ee7b5d9635c5c5d05577fb23f918a76db
goni715 <halesgoni715149123@gmail.com>
2025-11-17 20:01:20 +0600

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    //host: '13.53.182.102',  
     host: '0.0.0.0'    
   },
-  preview: { 
-    allowedHosts: ['dashboard.machmakers.co.uk', '13.53.182.102', "www.dashboard.machmakers.co.uk"],  
+  preview: {
+    allowedHosts: ['13.61.205.202'],
   },
 })
