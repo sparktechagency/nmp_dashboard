4a2da1a84675efdd0e4b33532c533e425d73cb5b
goni715 <halesgoni715149123@gmail.com>
2025-11-19 09:57:52 +0600

    update category page

--- src/pages/dashboard/CategoryPage.tsx
+++ src/pages/dashboard/CategoryPage.tsx
@@ -1,14 +1,15 @@
+import CategoryList from "../../components/category/CategoryList"
 
 const CategoryPage = () => {
   return (
     <>
-       {/* <div>
+       <div>
         <div className="bg-white shadow rounded-lg h-full overflow-hidden">
           <div className="w-full h-full flex flex-col">
             <CategoryList/>
           </div>
         </div>
-      </div> */}
+      </div>
     </>
   )
 }
